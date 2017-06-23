ee9eb253036b322702139e21d2fd158623f29534
Jix <amazon.n8@gmail.com>
2017-06-23 14:59:26 +0700

    :green_heart: CHANGE SERVER LISTEN PORT FOR HEROKU BUILD

--- app.js
+++ app.js
@@ -75,6 +75,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 module.exports = app;
